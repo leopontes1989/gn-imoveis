5e645fec1a82033753c41c8210805efe41e0e4c5
Leo Pontes <leopontes.1989@gmail.com>
2023-02-23 23:33:35 -0200

    repair navbar icon

--- navbar.js
+++ navbar.js
@@ -1,13 +1,15 @@
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    var y = document.querySelector(".active");
+    let x = document.getElementById("myLinks");
+    let y = document.querySelector(".active");
+
     if (x.style.display === "block") {
       x.style.display = "none";
-      y.style.display = "block"
+      y.style.display = "block";
+
+
     } else {
       x.style.display = "block";
       y.style.display = "none";
-      
     }
 
     
