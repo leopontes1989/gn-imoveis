4f4860150d9217c42c8847c4ab05330c16322b25
Leonardo Pontes <leopontes.1989@gmail.com>
2023-03-02 17:15:58 -0300

    alimentação parcial de conteúdo das paginas de imoveis

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+
+// window.onload = function() {
+//   showLoading();
+// }
+
 function myFunction() {
     let x = document.getElementById("myLinks");
     let y = document.querySelector(".active");
---
+++ js/loading.js
@@ -0,0 +1,8 @@
+// function showLoading() {
+//     setTimeout(() => hideLoading(), 2000);
+// }
+
+function hideLoading() {
+    const ploader = document.getElementById('preloader');
+    ploader.remove();
+}
