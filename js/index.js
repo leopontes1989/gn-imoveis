7ddf180d0b0074459f7e2d8eb350a5adad898155
Leonardo Pontes <leopontes.1989@gmail.com>
2023-03-08 11:33:03 -0300

    Responsive pages done

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@
 function myFunction() {
     let x = document.getElementById("myLinks");
     let y = document.querySelector(".active");
+    let z = document.querySelector(".video");
 
     if (x.style.display === "block") {
       x.style.display = "none";
@@ -19,3 +20,29 @@ function myFunction() {
 
     
 }
+
+// Menu Hamburguer
+
+function onClickMenu() {
+  document.getElementById('menu').classList.toggle('change');
+  document.getElementById('nav').classList.toggle('change');
+  document.getElementById('menu-bg').classList.toggle('change-bg');
+}
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(evt) {
+  if (evt.type === 'touchstart') evt.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  evt.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+      evt.currentTarget.setAttribute('aria-label', 'Fechar menu');
+  } else {
+      evt.currentTarget.setAttribute('aria-label', 'Fechar menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
